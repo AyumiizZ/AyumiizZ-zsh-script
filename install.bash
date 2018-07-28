e39caa5dac459d37d948698fa458e9b67e001ab9
korrawitc <korrawitc@gmail.com>
2018-07-28 07:57:48 +0700

    update

--- install.bash
+++ install.bash
@@ -4,14 +4,17 @@ echo "============ INSTALL ZSH ==========="
 sudo apt-get -y install zsh
 echo "============ INSTALL GIT ==========="
 sudo apt-get -y install git
-echo "============ CLONE GIT ============="
-cd ~
-git clone https://github.com/AyumiizZ/zsh-install.git
-cd zsh-install
+echo "======== INSTALL OH MY ZSH ========="
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+cd ~/.oh-my-zsh
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+cd -
+echo "=========== EDIT ZSHRC ============="
 cat .zshrc_head > .zshrc
 echo "export ZSH=/home/"$USER"/.oh-my-zsh" >> .zshrc
 cat .zshrc_tail >> .zshrc
 cp --recursive .oh-my-zsh .zshrc ~/.
+echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
 
 echo -n "\033c"
 
