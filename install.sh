e922be90caad1d3cdd3e90eddbe9a62e7ef0871d
korrawitc <korrawitc@gmail.com>
2018-08-10 14:51:15 +0700

    update

--- install.sh
+++ install.sh
@@ -21,7 +21,8 @@ echo "export ZSH=/home/"$USER"/.oh-my-zsh" >> .zshrc
 cat .zshrc_tail >> .zshrc
 echo "source /home/"$USER"/.oh-my-zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> .zshrc
 cp --recursive .oh-my-zsh .zshrc ~/.
-
+cd ~
+rm -rf AyumiizZ-zsh-script
 
 echo -n "\033c"
 
