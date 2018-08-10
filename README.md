8269e9c690cb1967bc6e2213b4217e5c033971ba
korrawitc <korrawitc@gmail.com>
2018-08-10 15:29:06 +0700

    update

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # AyumiizZ-zsh-script
 
-sh -c "$(wget https://raw.githubusercontent.com/AyumiizZ/AyumiizZ-zsh-script/master/install.sh -O -)"
+### Basic Installation
+#### via wget
+```shell
+sh -c "$(wget https://raw.githubusercontent.com/AyumiizZ/AyumiizZ-zsh-script/one-line/install.sh -O -)"
+```
--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 main(){
-cd ~
 echo "============== UPDATE =============="
 sudo apt-get -y update
 echo "============ INSTALL ZSH ==========="
@@ -9,19 +8,15 @@ sudo apt-get -y install git
 echo "======== INSTALL OH MY ZSH ========="
 rm -rf ~/.oh-my-zsh ~/.zshrc
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-cd ~/.oh-my-zsh
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/zsh-syntax-highlighting
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 echo "=========== EDIT ZSHRC ============="
-cd ~
-git clone https://github.com/AyumiizZ/AyumiizZ-zsh-script.git
-cd ~/AyumiizZ-zsh-script
 cat .zshrc_head > .zshrc
 echo "export ZSH=/home/"$USER"/.oh-my-zsh" >> .zshrc
 cat .zshrc_tail >> .zshrc
 echo "source /home/"$USER"/.oh-my-zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> .zshrc
 cp --recursive .oh-my-zsh .zshrc ~/.
-cd ~
+cd ../
 rm -rf AyumiizZ-zsh-script
 
 # echo -n "\033c"
