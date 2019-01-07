79ddd1e2d2cfdd25442fea33e4362b3be04927ba
AyumiizZ <Korrawitc@gmail.com>
2019-01-08 02:56:48 +0700

    :sparkles: add wget scripts

---
+++ one-line/install.sh
@@ -0,0 +1,30 @@
+main(){
+echo "============== UPDATE =============="
+sudo apt-get -y update
+echo "======= INSTALL ZSH AND GIT ========"
+sudo apt-get -y install zsh git
+echo "======== INSTALL OH MY ZSH ========="
+rm -rf ~/.oh-my-zsh ~/.zshrc
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/zsh-syntax-highlighting
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+echo "=========== EDIT ZSHRC ============="
+echo "export ZSH="$HOME"/.oh-my-zsh" > ~/.zshrc
+cat AyumiizZ-zsh-script/.zshrc >> ~/.zshrc
+if ![ -f ~/.zsh_aliases -o -d ~/.zsh_aliases ];
+    touch ~/.zsh_aliases
+fi
+echo "source "$HOME"/.oh-my-zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
+cp --recursive AyumiizZ-zsh-script/.zshrc ~/.
+rm -rf AyumiizZ-zsh-script
+
+echo "======= CHANGE DEFAULT SHELL ======="
+sudo chsh -s /bin/zsh $USER
+
+echo "============ START ZSH ============="
+echo "======== ENJOY YOUR ZSH :) ========="
+echo "======= github.com/AyumiizZ ========"
+zsh
+}
+main
+
