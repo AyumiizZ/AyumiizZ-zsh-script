29a0902637f89f86c08973c55ea61d1b274f82ae
AyumiizZ <Korrawitc@gmail.com>
2019-01-08 18:10:07 +0700

    :bug: fix one line to clone git

--- one-line/install.sh
+++ one-line/install.sh
@@ -9,13 +9,16 @@ git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/zsh-syntax-highlighting
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 echo "=========== EDIT ZSHRC ============="
+git clone https://github.com/AyumiizZ/AyumiizZ-zsh-script.git ~/AyumiizZ-zsh-script
+cd ~/AyumiizZ-zsh-script
 echo "export ZSH="$HOME"/.oh-my-zsh" > ~/.zshrc
 cat .zshrc >> ~/.zshrc
 if ! [ -f ~/.zsh_aliases -o -d ~/.zsh_aliases ]; then
     touch ~/.zsh_aliases
 fi
 echo "source "$HOME"/.oh-my-zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
-cp --recursive .zshrc ~/.
+cd ~
+rm -rf AyumiizZ-zsh-script
 
 echo "======= CHANGE DEFAULT SHELL ======="
 sudo chsh -s /bin/zsh $USER
